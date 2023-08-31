d2a4a93218c2b34cb6bd5fd67f352af2dca409ce
Denis Korablev <info@codeweber.com>
2023-08-31 15:35:35 +0300

    hero_block_4

--- templates/flexible-content/child_blocks/hero_banner_4_1/hero_banner_4_1.php
+++ templates/flexible-content/child_blocks/hero_banner_4_1/hero_banner_4_1.php
@@ -1,95 +1,149 @@
 <?php
 
 /**
- * Hero 3_1
+ * Hero 4_1
  */
-$block = new CW_Settings(
-  $cw_settings = array(
-    'title' => 'I\'m User Interface Designer & Developer.',
-    'patternTitle' => '<h1 class="display-1 mb-5">%s</h1>',
-
-    'buttons' => '<div class="d-flex justify-content-center justify-content-lg-start flex-wrap flex-wrap" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900"><span><a href="#" class="btn btn-lg btn-primary rounded-pill me-2">See My Works</a></span><span><a href="#" class="btn btn-lg btn-outline-primary rounded-pill">Contact Me</a></span></div>',
-    'buttons_pattern' => '<div class="d-flex justify-content-center justify-content-lg-start flex-wrap flex-wrap" data-cues="slideInDown" data-group="page-title-buttons" data-delay="900">%s</div>',
-
-    'background_class_default' => 'wrapper bg-gray',
-
-    'divider' => true,
-
-    'swiper' => array(
-      'swiper_container_class' => 'w-100',
-      'image_class' => 'w-auto',
-      'wrapper_image_class' => '',
-      'image_pattern' => '<figure %5$s %9$s>%6$s<img %4$s src="%1$s" srcset="%1$s" %3$s />%7$s %10$s %11$s</figure>',
-      'image_thumb_size' => 'sandbox_hero_3',
-      'image_demo' => '<div class="img-mask mask-1"><img src="' . get_template_directory_uri() . '/dist/img/photos/about17.jpg" srcset="' . get_template_directory_uri() . '/dist/img/photos/about17@2x.jpg 2x" alt="" /></div><div class="card shadow-lg position-absolute" style="bottom: 10%; right: 2%;"><div class="card-body py-4 px-5"><div class="d-flex flex-row align-items-center"><div><img src="' . get_template_directory_uri() . '/dist/img/icons/lineal/check.svg" class="svg-inject icon-svg icon-svg-sm text-primary mx-auto me-3" alt="" /></div><div><h3 class="counter mb-0 text-nowrap">250+</h3><p class="fs-14 lh-sm mb-0 text-nowrap">Projects Done</p></div></div></div><!--/.card-body --></div><!--/.card -->',
-      'image_big_size' => 'project_1',
-      'img_link' => '/dist/img/photos/about17.jpg',
-      'data_margin' => '30',
-      'nav' => 'true',
-      'nav_color' => NULL,
-      'nav_position' => NULL,
-      'dots' => 'false',
-      'dots_color' => NULL,
-      'dots_position' => NULL,
-      'swiper_effect' => NULL,
-      'base_items' => '1',
-      'items_xs' => '1',
-      'items_sm' => '1',
-      'items_md' => '1',
-      'items_lg' => '1',
-      'items_xl' => '1',
-      'items_xxl' => '1',
-      'autoplay' => 'false',
-      'autoplay_time' => '3000',
-      'loop' => 'false',
-      'autoheight' => 'false',
-      'image_shape' => 'img-mask mask-1',
-    ),
-
-    'label_demo' => '<div class="card shadow-lg" style="bottom: 5rem; right: 5rem;"><div class="card-body py-4 px-5"><div class="d-flex flex-row align-items-center"><div><img src="' . get_template_directory_uri() . '/dist/img/icons/lineal/check.svg" class="svg-inject icon-svg icon-svg-sm text-primary mx-auto me-3" alt="" /></div><div><h3 class="counter mb-0 text-nowrap">250+</h3><p class="fs-14 lh-sm mb-0 text-nowrap">Projects Done</p></div></div></div><!--/.card-body --></div><!--/.card -->',
-
-    'label_pattern' => '<div class="card shadow-lg position-absolute zindex-1 %6$s" %7$s><div class="card-body py-4 px-5"><div class="d-flex flex-row align-items-center"><div>%2$s</div><div><div class="counter mb-0 text-nowrap h3">%3$s</div><p class="fs-14 lh-sm mb-0 text-nowrap">%4$s</p>%5$s</div></div></div><!--/.card-body --></div><!--/.card -->',
-
-    'features' => '<div class="col-12"><div class="d-flex flex-row"><div><div class="icon btn btn-circle btn-lg btn-soft-primary disabled me-5"><i class="uil uil-phone-volume"></i> </div></div><div><div class="mb-1 h4">24/7 Support</div><p class="mb-0">Nulla vitae elit libero pharetra augue dapibus.</p></div></div></div><!--/column -->',
-    'features_pattern' => '<div class="col-12 %1$s"><div class="d-flex flex-row"><div>%2$s</div><div><div class="h4 mb-1%9$s">%3$s</div><p class="mb-0%10$s">%4$s</p>%5$s</div></div></div><!--/column -->',
-    'features_style_icon' => 'disabled me-5',
-
-    'column_class_1' => '',
-    'column_class_2' => 'order-lg-2 offset-lg-1',
-  )
-);
 ?>
 
-<section id="<?php echo esc_html($args['block_id']); ?>" class="<?php echo $block->section_class; ?> <?php page_frame_banner(); ?> <?php echo esc_html($args['block_class']); ?>" <?php echo $block->background_data; ?>>
-  <div class="container pt-12 pt-md-14 pb-14 pb-md-16">
-    <div class="row gy-10 gy-md-13 gy-lg-0 align-items-center">
-      <div class="col-md-8 col-lg-5 d-flex position-relative mx-auto <?php echo $block->column_class_1; ?>" data-cues="slideInDown" data-group="header">
-        <?php echo $block->swiper_final; ?>
-        <!--/swiper -->
+
+<section id="<?php echo esc_html($args['block_id']); ?>" class="wrapper bg-light <?php page_frame_banner(); ?> <?php echo esc_html($args['block_class']); ?>">
+  <div class="container py-14 py-md-16">
+    <div class="row gx-lg-8 gx-xl-12 gy-10 align-items-center" data-cues="slideInDown" data-delay="600">
+      <div class="col-lg-6 position-relative order-2 order-lg-2">
+        <div class="shape bg-dot primary rellax w-16 h-20" data-rellax-speed="1" style="top: 3rem; left: 5.5rem"></div>
+        <div class="overlap-grid overlap-grid-2">
+          <div class="item">
+            <figure class="rounded shadow"><img src="https://placehold.co/800x800" srcset="https://placehold.co/800x800" alt=""></figure>
+          </div>
+          <div class="item">
+            <div class="rounded shadow bg-white">
+              <table class="table table-borderless ">
+                <tbody>
+                  <tr>
+                    <td>Стойкость принта</td>
+                    <td class="text-sky">
+                      <svg width="120" height="24" viewBox="0 0 120 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12.56 2.17004C12.3946 2.05827 12.1996 1.99854 12 1.99854C11.8004 1.99854 11.6054 2.05827 11.44 2.17004C11.14 2.37004 4.25 7.17004 4.25 14.25C4.25 16.3055 5.06652 18.2767 6.51992 19.7301C7.97333 21.1835 9.94457 22 12 22C14.0554 22 16.0267 21.1835 17.4801 19.7301C18.9335 18.2767 19.75 16.3055 19.75 14.25C19.75 7.05004 12.85 2.36004 12.56 2.17004Z" fill="#6563FF" />
+                        <path d="M36.56 2.17004C36.3946 2.05827 36.1996 1.99854 36 1.99854C35.8004 1.99854 35.6054 2.05827 35.44 2.17004C35.14 2.37004 28.25 7.17004 28.25 14.25C28.25 16.3055 29.0665 18.2767 30.5199 19.7301C31.9733 21.1835 33.9446 22 36 22C38.0554 22 40.0267 21.1835 41.4801 19.7301C42.9335 18.2767 43.75 16.3055 43.75 14.25C43.75 7.05004 36.85 2.36004 36.56 2.17004Z" fill="#6563FF" />
+                        <path d="M60.56 2.17004C60.3946 2.05827 60.1996 1.99854 60 1.99854C59.8004 1.99854 59.6054 2.05827 59.44 2.17004C59.14 2.37004 52.25 7.17004 52.25 14.25C52.25 16.3055 53.0665 18.2767 54.5199 19.7301C55.9733 21.1835 57.9446 22 60 22C62.0554 22 64.0267 21.1835 65.4801 19.7301C66.9335 18.2767 67.75 16.3055 67.75 14.25C67.75 7.05004 60.85 2.36004 60.56 2.17004Z" fill="#6563FF" />
+                        <path d="M84.56 2.17004C84.3946 2.05827 84.1996 1.99854 84 1.99854C83.8004 1.99854 83.6054 2.05827 83.44 2.17004C83.14 2.37004 76.25 7.17004 76.25 14.25C76.25 16.3055 77.0665 18.2767 78.5199 19.7301C79.9733 21.1835 81.9446 22 84 22C86.0554 22 88.0267 21.1835 89.4801 19.7301C90.9335 18.2767 91.75 16.3055 91.75 14.25C91.75 7.05004 84.85 2.36004 84.56 2.17004Z" fill="#6563FF" />
+                        <path d="M108.56 2.17004C108.395 2.05827 108.2 1.99854 108 1.99854C107.8 1.99854 107.605 2.05827 107.44 2.17004C107.14 2.37004 100.25 7.17004 100.25 14.25C100.25 16.3055 101.067 18.2767 102.52 19.7301C103.973 21.1835 105.945 22 108 22C110.055 22 112.027 21.1835 113.48 19.7301C114.933 18.2767 115.75 16.3055 115.75 14.25C115.75 7.05004 108.85 2.36004 108.56 2.17004Z" fill="#6563FF" />
+                      </svg>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Скорость печати
+                    </td>
+                    <td>
+                      <svg width="120" height="24" viewBox="0 0 120 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12.56 2.17004C12.3946 2.05827 12.1996 1.99854 12 1.99854C11.8004 1.99854 11.6054 2.05827 11.44 2.17004C11.14 2.37004 4.25 7.17004 4.25 14.25C4.25 16.3055 5.06652 18.2767 6.51992 19.7301C7.97333 21.1835 9.94457 22 12 22C14.0554 22 16.0267 21.1835 17.4801 19.7301C18.9335 18.2767 19.75 16.3055 19.75 14.25C19.75 7.05004 12.85 2.36004 12.56 2.17004Z" fill="#6563FF" />
+                        <path d="M36.56 2.17004C36.3946 2.05827 36.1996 1.99854 36 1.99854C35.8004 1.99854 35.6054 2.05827 35.44 2.17004C35.14 2.37004 28.25 7.17004 28.25 14.25C28.25 16.3055 29.0665 18.2767 30.5199 19.7301C31.9733 21.1835 33.9446 22 36 22C38.0554 22 40.0267 21.1835 41.4801 19.7301C42.9335 18.2767 43.75 16.3055 43.75 14.25C43.75 7.05004 36.85 2.36004 36.56 2.17004Z" fill="#6563FF" />
+                        <path d="M60.56 2.17004C60.3946 2.05827 60.1996 1.99854 60 1.99854C59.8004 1.99854 59.6054 2.05827 59.44 2.17004C59.14 2.37004 52.25 7.17004 52.25 14.25C52.25 16.3055 53.0665 18.2767 54.5199 19.7301C55.9733 21.1835 57.9446 22 60 22C62.0554 22 64.0267 21.1835 65.4801 19.7301C66.9335 18.2767 67.75 16.3055 67.75 14.25C67.75 7.05004 60.85 2.36004 60.56 2.17004Z" fill="#6563FF" />
+                        <path d="M84.56 2.17004C84.3946 2.05827 84.1996 1.99854 84 1.99854C83.8004 1.99854 83.6054 2.05827 83.44 2.17004C83.14 2.37004 76.25 7.17004 76.25 14.25C76.25 16.3055 77.0665 18.2767 78.5199 19.7301C79.9733 21.1835 81.9446 22 84 22C86.0554 22 88.0267 21.1835 89.4801 19.7301C90.9335 18.2767 91.75 16.3055 91.75 14.25C91.75 7.05004 84.85 2.36004 84.56 2.17004Z" fill="#6563FF" />
+                        <path d="M108.56 2.17004C108.395 2.05827 108.2 1.99854 108 1.99854C107.8 1.99854 107.605 2.05827 107.44 2.17004C107.14 2.37004 100.25 7.17004 100.25 14.25C100.25 16.3055 101.067 18.2767 102.52 19.7301C103.973 21.1835 105.945 22 108 22C110.055 22 112.027 21.1835 113.48 19.7301C114.933 18.2767 115.75 16.3055 115.75 14.25C115.75 7.05004 108.85 2.36004 108.56 2.17004Z" fill="#6563FF" />
+                      </svg>
+                    </td>
+
+                  </tr>
+                  <tr>
+                    <td>
+                      Скорость печати
+                    </td>
+                    <td>
+                      <svg width="120" height="24" viewBox="0 0 120 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12.56 2.17004C12.3946 2.05827 12.1996 1.99854 12 1.99854C11.8004 1.99854 11.6054 2.05827 11.44 2.17004C11.14 2.37004 4.25 7.17004 4.25 14.25C4.25 16.3055 5.06652 18.2767 6.51992 19.7301C7.97333 21.1835 9.94457 22 12 22C14.0554 22 16.0267 21.1835 17.4801 19.7301C18.9335 18.2767 19.75 16.3055 19.75 14.25C19.75 7.05004 12.85 2.36004 12.56 2.17004Z" fill="#6563FF" />
+                        <path d="M36.56 2.17004C36.3946 2.05827 36.1996 1.99854 36 1.99854C35.8004 1.99854 35.6054 2.05827 35.44 2.17004C35.14 2.37004 28.25 7.17004 28.25 14.25C28.25 16.3055 29.0665 18.2767 30.5199 19.7301C31.9733 21.1835 33.9446 22 36 22C38.0554 22 40.0267 21.1835 41.4801 19.7301C42.9335 18.2767 43.75 16.3055 43.75 14.25C43.75 7.05004 36.85 2.36004 36.56 2.17004Z" fill="#6563FF" />
+                        <path d="M60.56 2.17004C60.3946 2.05827 60.1996 1.99854 60 1.99854C59.8004 1.99854 59.6054 2.05827 59.44 2.17004C59.14 2.37004 52.25 7.17004 52.25 14.25C52.25 16.3055 53.0665 18.2767 54.5199 19.7301C55.9733 21.1835 57.9446 22 60 22C62.0554 22 64.0267 21.1835 65.4801 19.7301C66.9335 18.2767 67.75 16.3055 67.75 14.25C67.75 7.05004 60.85 2.36004 60.56 2.17004Z" fill="#6563FF" />
+                        <path d="M84.56 2.17004C84.3946 2.05827 84.1996 1.99854 84 1.99854C83.8004 1.99854 83.6054 2.05827 83.44 2.17004C83.14 2.37004 76.25 7.17004 76.25 14.25C76.25 16.3055 77.0665 18.2767 78.5199 19.7301C79.9733 21.1835 81.9446 22 84 22C86.0554 22 88.0267 21.1835 89.4801 19.7301C90.9335 18.2767 91.75 16.3055 91.75 14.25C91.75 7.05004 84.85 2.36004 84.56 2.17004Z" fill="#6563FF" />
+                        <path d="M108.56 2.17004C108.395 2.05827 108.2 1.99854 108 1.99854C107.8 1.99854 107.605 2.05827 107.44 2.17004C107.14 2.37004 100.25 7.17004 100.25 14.25C100.25 16.3055 101.067 18.2767 102.52 19.7301C103.973 21.1835 105.945 22 108 22C110.055 22 112.027 21.1835 113.48 19.7301C114.933 18.2767 115.75 16.3055 115.75 14.25C115.75 7.05004 108.85 2.36004 108.56 2.17004Z" fill="#6563FF" />
+                      </svg>
+                    </td>
+
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
       <!--/column -->
-      <div class="col-lg-5 text-center text-lg-start <?php echo $block->column_class_2; ?>" data-cues="slideInDown" data-group="page-title" data-delay="600">
-        <?php echo $block->title; ?>
-        <!--/title -->
-        <?php // echo $block->paragraph; 
-        ?>
-        <!--/pargraph -->
-        <div class="row gx-xl-10 gy-6 mb-6 text-start">
-          <?php echo $block->features; ?>
-          <!--/features -->
+      <div class="col-lg-6 order-1">
+        <h2 class="display-5 mb-3">Прямая печать</h2>
+        <p class="lead fs-md">Прямая печать имеет ряд преимуществ перед другими методами переноса изображения на ткань.</p>
+        <div class="row">
+          <div class="col-12">
+            <ul class="icon-list bullet-bg bullet-primary">
+              <li><span><i class="uil uil-check"></i></span><span>Возможность печати любого полноцветного изображения</span></li>
+              <li><span><i class="uil uil-check"></i></span><span>Принт печатается за 15-20 минут, в зависимости от размера нанесения</span></li>
+              <li><span><i class="uil uil-check"></i></span><span>Устойчивость изображения к стиркам и механическому воздействию</span></li>
+              <li><span><i class="uil uil-check"></i></span><span>Позволяет печатать любые тиражи и единичные экземпляры</span></li>
+            </ul>
+          </div>
         </div>
-        <!--/.row -->
-        <?php echo $block->buttons; ?>
-        <!--/buttons group -->
+        <h3 class="display-7 mb-3">Галерея работ</h3>
+        <div class="swiper-container mb-10 swiper-container-0" data-margin="30" data-nav="true" data-dots="true" data-items-xl="4" data-items-md="3" data-items-xs="2">
+          <div class="swiper swiper-initialized swiper-horizontal swiper-pointer-events swiper-backface-hidden">
+            <div class="swiper-wrapper" id="swiper-wrapper-b7b109d1af9102697a" aria-live="off" style="cursor: grab; transform: translate3d(-533.333px, 0px, 0px); transition-duration: 0ms;">
+              <div class="swiper-slide" role="group" aria-label="1 / 5" style="width: 236.667px; margin-right: 30px;">
+                <figure class="overlay overlay-1 hover-scale rounded mb-0"><a href="#"> <img src="https://placehold.co/800x800" srcset="https://placehold.co/800x800" alt=""><span class="bg"></span></a>
+                  <figcaption>
+                    <h5 class="from-top mb-0">Read More</h5>
+                  </figcaption>
+                </figure>
+              </div>
+              <!--/.swiper-slide -->
+              <div class="swiper-slide swiper-slide-prev" role="group" aria-label="2 / 5" style="width: 236.667px; margin-right: 30px;">
+                <figure class="overlay overlay-1 hover-scale rounded mb-0"><a href="#"> <img src="https://placehold.co/800x800" srcset="https://placehold.co/800x800" alt=""><span class="bg"></span></a>
+                  <figcaption>
+                    <h5 class="from-top mb-0">Read More</h5>
+                  </figcaption>
+                </figure>
+              </div>
+              <!--/.swiper-slide -->
+              <div class="swiper-slide swiper-slide-active" role="group" aria-label="3 / 5" style="width: 236.667px; margin-right: 30px;">
+                <figure class="overlay overlay-1 hover-scale rounded mb-0"><a href="#"> <img src="https://placehold.co/800x800" srcset="https://placehold.co/800x800" alt=""><span class="bg"></span></a>
+                  <figcaption>
+                    <h5 class="from-top mb-0">Read More</h5>
+                  </figcaption>
+                </figure>
+              </div>
+              <!--/.swiper-slide -->
+              <div class="swiper-slide swiper-slide-next" role="group" aria-label="4 / 5" style="width: 236.667px; margin-right: 30px;">
+                <figure class="overlay overlay-1 hover-scale rounded mb-0"><a href="#"> <img src="https://placehold.co/800x800" srcset="https://placehold.co/800x800" alt=""><span class="bg"></span></a>
+                  <figcaption>
+                    <h5 class="from-top mb-0">Read More</h5>
+                  </figcaption>
+                </figure>
+              </div>
+              <!--/.swiper-slide -->
+              <div class="swiper-slide" role="group" aria-label="5 / 5" style="width: 236.667px; margin-right: 30px;">
+                <figure class="overlay overlay-1 hover-scale rounded mb-0"><a href="#"> <img src="https://placehold.co/800x800" srcset="https://placehold.co/800x800" alt=""><span class="bg"></span></a>
+                  <figcaption>
+                    <h5 class="from-top mb-0">Read More</h5>
+                  </figcaption>
+                </figure>
+              </div>
+              <!--/.swiper-slide -->
+            </div>
+            <!--/.swiper-wrapper -->
+            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+          </div>
+          <!-- /.swiper -->
+          <div class="swiper-controls">
+            <div class="swiper-navigation">
+              <div class="swiper-button swiper-button-prev" tabindex="0" role="button" aria-label="Previous slide" aria-controls="swiper-wrapper-b7b109d1af9102697a" aria-disabled="false"></div>
+              <div class="swiper-button swiper-button-next swiper-button-disabled" tabindex="-1" role="button" aria-label="Next slide" aria-controls="swiper-wrapper-b7b109d1af9102697a" aria-disabled="true"></div>
+            </div>
+            <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal"><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 1"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 2"></span><span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 3" aria-current="true"></span></div>
+          </div>
+        </div>
+
       </div>
       <!--/column -->
     </div>
-    <!-- /.row -->
+    <!--/.row -->
   </div>
   <!-- /.container -->
-  <?php if ($block->divider_wave) {
-    echo $block->divider_wave;
-  } ?>
-  <!-- /divider -->
 </section>
 <!-- /section -->
